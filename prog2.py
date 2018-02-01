a9d7cad0d49afb2ccda9094fb9d55e41bb5a3a91
die4700 <die4700@calu.edu>
2018-02-01 13:45:35 -0500

    Added loop

--- prog2.py
+++ prog2.py
@@ -1,1 +1,8 @@
 print("Hello world!")
+
+numRun = int(input("Enter the number of times you would like the loop to run:"))
+counter = 0
+
+for i in range (0,numRun):
+    counter = counter + 1
+    print("Loop has run ", counter, "time(s)!")
